b9e5f65d25f565cce3b1a27c0898fb44b5b46d43
soraabunaser <110615314+soraabunaser@users.noreply.github.com>
2023-03-09 18:44:15 +0200

    Initial changes

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.scss'
+
+function App() {
+  return (
+    <div className="app">
+
+      <header className="app__header">
+      </header>
+
+      <main className="app__main">
+      </main>
+
+    </div>
+  );
+}
+
+
+export default App;
